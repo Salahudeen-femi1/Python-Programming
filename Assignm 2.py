6721197cf72736a31e114171c3bb85c1ae244396
Salahudeen-femi1 <102353790+Salahudeen-femi1@users.noreply.github.com>
2022-05-25 10:52:22 +0800

    Add files via upload
    ASSIGNMENT DAY1

---
+++ Assignm 2.py
@@ -0,0 +1,5 @@
+#CONVERSION FROM DEGREE CELCIUS TO FERHEIT
+degree_celcisu = float(input('what is the value in degree celcius? '))
+degree_ferheit = float(input('what is the degree of ferheit? '))
+
+print(degree_celcisu * 1.8 + 32)
